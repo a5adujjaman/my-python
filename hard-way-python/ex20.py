be49875d1806ebb8c4490fb9cc38893ade9ca296
a5adujjaman <asadujjamanasif46567@gmail.com>
2024-10-05 19:18:50 +0600

    first commit

--- hard-way-python/ex20.py
+++ hard-way-python/ex20.py
@@ -7,8 +7,8 @@ script, input_file = argv
 def print_all(f):
         print(f.read())
 def rewind(f):
-        f.seek(0)
-def print a_line(line_count, f):
+        f.seek(0) # seek funciton is dealing in bytes
+def print_a_line(line_count, f):
         print(line_count, f.readline())
 
 current_file = open(input_file)
